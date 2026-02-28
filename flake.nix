21d5140a1b00fe97ab6c075ac0277806e2b37edb
Vinetos <contact+git@vinetos.fr>
2026-02-28 10:46:56 +0100

    feat: Update flake inputs

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     flake-parts.url = "github:hercules-ci/flake-parts";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:Vinetos/nixpkgs/stestr-check-hook";
   };
 
   outputs =
